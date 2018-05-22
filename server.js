fc5ed30858680fe8c47965d3c15bbba7024bd479
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-05-22 12:24:17 -0700

    Added HTML

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+
+
+var app = express()
+
+var PORT = process.env.PORT || 8080
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+require("./routes/apiRoutes")(app)
+require("./routes/htmlRoutes")(app)
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT)
+})
